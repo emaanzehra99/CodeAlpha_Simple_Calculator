5a21154ad8f7354ac3bf82dd507f5a40a6ccc98b
emaanzehra99 <emaanzehra.swips@gmail.com>
2024-11-25 15:36:17 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-function calculate() {
+// Calculator Program 
+
+const display = document.getElementById("display");
+
+//To display values.
+
+function appendDisplay(input) {
+    display.value += input;
+}
+
+//To calculate results and to generate an error if user enters incomplete value and hit '='.
+
+function calcResult() {
     try {
         display.value = eval(display.value);
     }
     catch(error) {
-        display.value = "Error!";
+        display.value = "Try Again!"
     }
+}
+
+//To clear Display.
+
+function clearDisplay() {
+    display.value = "";
 }
